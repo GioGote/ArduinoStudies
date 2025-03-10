e403e2c1cd52c239076a8df1095ec9cf1e6fcfc0
Gio Velasco <sivelasco.cs@gmail.com>
2025-03-10 15:26:31 -0400

    cleanup

--- LEDBlinks/LEDBlinks.ino
+++ LEDBlinks/LEDBlinks.ino
@@ -1,50 +1,40 @@
-// Code meant to be able for a closed circuit to light a RED, GREEN, and BLUE LED.
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(8, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
 }
-
+ 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  delay(100);
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  delay(100);
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  delay(100);
-
-  digitalWrite(8, HIGH);
-  delay(500);
-  digitalWrite(8, LOW);
-  delay(500);
-  digitalWrite(8, HIGH);
-  delay(500);
-  digitalWrite(8, LOW);
-  delay(500);
-  digitalWrite(8, HIGH);
-  delay(500);
-  digitalWrite(8, LOW);
-  delay(500);
-
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  delay(100);
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  delay(100);
-  digitalWrite(8, HIGH);
-  delay(100);
-  digitalWrite(8, LOW);
-  delay(1000);
+  redBlinks();
+  blueBlinks();
+  greenBlinks();
+}
+ 
+void redBlinks() {
+  for (int i = 0; i < 5; i++){
+    digitalWrite(8, HIGH);
+    delay(200);
+    digitalWrite(8, LOW);
+    delay(200);
+  }
+}
+ 
+void blueBlinks() {
+  for (int i = 0; i < 10; i++){
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+  }
+}
+ 
+void greenBlinks() {
+  for (int i = 0; i < 15; i++){
+    digitalWrite(12, HIGH);
+    delay(200);
+    digitalWrite(12, LOW);
+    delay(200);
+  }
 }
